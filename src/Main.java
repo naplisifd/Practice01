7561a7a8b633a00e4e899f2b3c0e46a987257b94
naplisifd <didajeglavni@gmail.com>
2024-02-15 21:10:39 +1000

    i6

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println("Hello and welcome!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 6; i++) {
 
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
